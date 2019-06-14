54aff22bd1c4cdf75ed58ea297dd265a295ba6e4
yakov <yakovburtsev@gmail.com>
2019-06-14 10:29:18 +0300

    prepare for progmatically add fragment to FrameLayout

--- app/src/main/java/com/yaburtsev/workout/MainActivity.java
+++ app/src/main/java/com/yaburtsev/workout/MainActivity.java
@@ -1,8 +1,8 @@
 package com.yaburtsev.workout;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
@@ -15,8 +15,13 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     @Override
     public void itemClicked(long id) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
-        startActivity(intent);
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if (fragmentContainer != null) {
+            //todo: add the fragment to the FrameLayout
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
+            startActivity(intent);
+        }
     }
 }
