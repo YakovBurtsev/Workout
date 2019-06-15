8691508384afaa8d28196584ce40e720a768f314
yakov <yakovburtsev@gmail.com>
2019-06-15 14:46:35 +0300

    add stopwatch fragment into details fragment

--- app/src/main/java/com/yaburtsev/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/yaburtsev/workout/WorkoutDetailFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +27,14 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
+        if (savedInstanceState == null) {
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, new StopwatchFragment());
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
+        else {
             workoutId = savedInstanceState.getInt(WORKOUT_ID);
         }
     }
